a4c403eb3760f1ed334d78fcb15a90559e14ce5f
irconde <ivanrodriguezconde@gmail.com>
2019-10-17 22:26:06 -0500

    appbar setup

---
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -0,0 +1,77 @@
+package edu.ualr.supplementarycontent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.favourite_action:
+                Log.d(TAG, "Favourite option clicked");
+                return true;
+            case R.id.delete_action:
+                Log.d(TAG, "Delete option clicked");
+                return true;
+            case R.id.settings_action:
+                Log.d(TAG, "Settings option clicked");
+                return true;
+                default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openChildActivity(View view) {
+        Intent intent = new Intent(this, ChildActivity.class);
+        startActivity(intent);
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = getResources().getString(R.string.toast_message);
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.TOP, 0, 128);
+        toast.show();
+    }
+
+    // TODO 01. Define a customized layout for the toast notification. custom_toast
+    public void showCustomToast(View view) {
+        // TODO 02. We retrieve the layout inflater
+        // TODO 03. Inflate the toast layout and create the corresponding view
+        // TODO 04. We set the text of the custom toast
+        // TODO 05. We can set toast's gravity and duration
+        // TODO 06. We plug in the view in the toast
+    }
+}
