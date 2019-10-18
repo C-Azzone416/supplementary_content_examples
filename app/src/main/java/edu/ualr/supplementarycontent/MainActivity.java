f9f3321b24f8069dd04a2487380fa490a18b9943
irconde <ivanrodriguezconde@gmail.com>
2019-10-17 22:26:06 -0500

    appbar setup

---
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -0,0 +1,64 @@
+package edu.ualr.supplementarycontent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.favourite_action:
+                Log.d(TAG, "Favourite option clicked");
+                return true;
+            case R.id.delete_action:
+                Log.d(TAG, "Delete option clicked");
+                return true;
+            case R.id.settings_action:
+                Log.d(TAG, "Settings option clicked");
+                return true;
+                default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openChildActivity(View view) {
+        Intent intent = new Intent(this, ChildActivity.class);
+        startActivity(intent);
+    }
+
+    public void showToast(View view) {
+        // TODO 01. Instantiate a Toast object.
+        // We must provide the application context, the text message, and the duration of the toast
+        // TODO 02. We define the position of the toast
+        // By default, it's positioned in the bottom center of the screen
+        // TODO 03. We display the toast notification
+    }
+}
